ff7427e284c1a6d1357dd13ba632fee17d37eb83
Ibrahim <ibraguim.karsamov@gmail.com>
2023-03-09 09:00:46 +0100

    Reste plus que le css

--- app.js
+++ app.js
@@ -1,13 +1,49 @@
-const gridsContainer = document.querySelector('#grids-container');
+let color = 'black';
+let click = true;
 
-gridsContainer.style.height = '500px';
-gridsContainer.style.width = '500px';
-gridsContainer.style.display = 'grid';
-gridsContainer.style.gridTemplateColumns = 'repeat(16, 1fr)';
-gridsContainer.style.gridTemplateRows = 'repeat(16, 1fr)';
+function divider(size) {
+    let gridsContainer = document.querySelector('#grids-container');
+    let grids = document.querySelectorAll('div.pixel')
+    grids.forEach((div) => div.style.backgroundColor = 'white');
+    gridsContainer.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+    gridsContainer.style.gridTemplateRows = `repeat(${size}, 1fr)`;
 
-for (let i = 0; i < 256; i++) {
-    const gridElement = document.createElement('div');
-    gridElement.classList = 'grid';
-    gridsContainer.appendChild(gridElement);
+    for (let i = 0; i < size * size; i++) {
+        const gridElement = document.createElement('div');
+        gridElement.classList = 'pixel';
+        gridsContainer.appendChild(gridElement);
+        gridElement.addEventListener('mouseover', colorGrid);
+    };
+};
+
+divider(16);
+
+function changeSize(input) {
+    divider(input);
+};
+
+function colorGrid() {
+    if (click) {
+        this.style.backgroundColor = color;
+    }
 }
+
+function changeColor(inputColor) {
+    color = inputColor;
+};
+
+function clearBoard() {
+    let grids = document.querySelectorAll('div.pixel')
+    grids.forEach((div) => div.style.backgroundColor = 'white');
+};
+
+document.querySelector('body').addEventListener('click', (e) => {
+    if (e.target.tagName != "BUTTON" && e.target.tagName != "INPUT") {
+        click = !click;
+        if (click) {
+            document.querySelector('#mode').textContent = 'Mode : Coloring';
+        } else {
+            document.querySelector('#mode').textContent = 'Mode : Not Coloring';
+        }
+    }
+})
